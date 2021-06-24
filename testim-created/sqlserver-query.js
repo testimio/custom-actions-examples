b5ffe393839e16c0f0f5133ebc6d8fc40032419e
Barry David Solomon <barrysolomon@hotmail.com>
2021-06-24 14:12:23 -0600

    Return first row of results as individual test level variables with  names matching the associated column(s)

--- testim-created/sqlserver-query.js
+++ testim-created/sqlserver-query.js
@@ -8,12 +8,20 @@
  *      returnVariableName (JS) [Optional] : Name of return data variable.  
  *              Uses queryResults if not defined
  * 	    connectionString (JS) [optional] : 'mssql://TestimSQL:wh4t3v3r4!@AuggieTheDoggie/TestData'
- *				You can also use config instead of connectionString
+ *				  You can also use config instead of connectionString
  *      sql  (npm)   : mssql@latest
  * 
  *  Returns
  *      queryResults or returnVariableName if defined
  * 
+ *      Return first row of results as individual test level variables with  names matching the associated column(s)
+ *  			
+ *		    For example, if the recordset contains data with the columns ["firstName", "lastName"], 
+ *
+ *          Two variables will be created: "firstName" and "lastName" with values that match the first set of data generated
+ *				    firstName === queryResults[0].firstName 
+ *			      lastName  === queryResults[0].lastName
+ *
  *  Base Step
  *      CLI Action
  * 
@@ -34,49 +42,63 @@
  */
 
  const config = {
-    user:     'TestimSQL',
-    password: 'wh4t3v3r4!',
-    server:   'AuggieTheDoggie', // You can use 'localhost\\instance' to connect to named instance
-    database: 'TestData',
-    options: {
+  user: 'TestimSQL',
+  password: 'wh4t3v3r4!',
+  server: 'AuggieTheDoggie', // You can use 'localhost\\instance' to connect to named instance
+  database: 'TestData',
+  options: {
       enableArithAbort: true,
       trustServerCertificate: true,
-    }
   }
-  
-  if (typeof connectionString === 'undefined' || connectionString === null) {
-      connectionString = config;
-  }
-  
-  if (typeof connectionString === 'undefined' || connectionString === null) {
-      throw new Error("connectionString is undefined.  Please set connectionString and try again.  Connection string is in the format 'mssql://username:password@localhost/database'");
-  }
-  
-  if (typeof sqlQuery === 'undefined' || sqlQuery === null) {
-      throw new Error("sqlQuery is undefined.  Please set sqlQuery and try again.");
-  }
-  
-  /* if returnVariableName is defined then use it else use 'queryResults' as the return variable
-   */
-  var return_variable_name = (typeof returnVariableName !== 'undefined' && returnVariableName !== null) ? returnVariableName : 'queryResults';
-  
-  return new Promise((resolve, reject) => {
-  
-      sql.connect(connectionString).then(() => {
-  
-        return sql.query (sqlQuery)
-  
-      }).then(result => {
-        
-        exportsTest[return_variable_name] = result;
-        console.log(return_variable_name, exportsTest[return_variable_name]);
-       
-        resolve();
-  
-      }).catch(err => {
-  
-        reject(err.message);
-  
-      })
-  
-  });
+}
+
+if (typeof connectionString === 'undefined' || connectionString === null) {
+  connectionString = config;
+}
+
+if (typeof connectionString === 'undefined' || connectionString === null) {
+  throw new Error("connectionString is undefined.  Please set connectionString and try again.  Connection string is in the format 'mssql://username:password@localhost/database'");
+}
+
+if (typeof sqlQuery === 'undefined' || sqlQuery === null) {
+  throw new Error("sqlQuery is undefined.  Please set sqlQuery and try again.");
+}
+
+/* if returnVariableName is defined then use it else use 'queryResults' as the return variable
+*/
+var return_variable_name = (typeof returnVariableName !== 'undefined' && returnVariableName !== null) ? returnVariableName : 'queryResults';
+
+return new Promise((resolve, reject) => {
+
+  sql.connect(connectionString).then(() => {
+
+      return sql.query(sqlQuery)
+
+  }).then(result => {
+
+      exportsTest[return_variable_name] = result;
+      console.log(return_variable_name, exportsTest[return_variable_name]);
+
+      // Take an index and store generatedData[0]'s values as naked top level variables
+      //
+      let naked_variable_index = 0;
+      function storeFirstAsGlobalNakedVariables(value) {
+
+          let variableName  = value;
+          let variableValue = result.recordset[naked_variable_index][variableName];
+
+          exportsTest[variableName] = variableValue;
+          console.log("variableName = " + variableValue);
+
+      }
+      Object.keys(result.recordset[naked_variable_index]).forEach(storeFirstAsGlobalNakedVariables);
+
+      resolve();
+
+  }).catch(err => {
+
+      reject(err.message);
+
+  })
+
+});
