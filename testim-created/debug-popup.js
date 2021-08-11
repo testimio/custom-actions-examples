ebe8511d7fb381cc5f8aa079977b6d5ddfa959e0
Barry David Solomon <barrysolomon@hotmail.com>
2021-08-11 14:40:25 -0600

    localize style td to target element

--- testim-created/debug-popup.js
+++ testim-created/debug-popup.js
@@ -12,7 +12,7 @@
  *                    If set then popup (and step) will block until timeout has expired.
  *     
  *  Note
- *      You can add a subsequesnt step that dismisses the popup to capture a screenshot
+ *      You can add a subsequent step that dismisses the popup to capture a screenshot
  * 
  *  Disclaimer
  *      This Custom Action is provided "AS IS".  It is for instructional purposes only and is not officially supported by Testim
@@ -43,7 +43,7 @@ document.getElementsByTagName('head')[0].appendChild(script_jquery);
 
 let style       = document.createElement('style');
 style.type      = 'text/css';
-style.innerHTML = 'td {overflow-y: scroll;max-width: 700px; word-wrap: break-word;} /* Popup box BEGIN */ .hover_bkgr_fricc{ background:rgba(0,0,0,.4); cursor:pointer; display:none; height:100%; position:fixed; text-align:center; top:-5px; width:100%; z-index:10000;}.hover_bkgr_fricc .helper{ display:inline-block; height:100%; vertical-align:middle;}.hover_bkgr_fricc > div {    background-color: #fff;    box-shadow: 10px 10px 60px #555; display: inline-block; height: auto; max-width: 1000px; min-height: 100px; vertical-align: middle; width: 60%; position: relative; border-radius: 8px; padding: 15px 5%;} .popupCloseButton {    background-color: #fff;    border: 3px solid #999;    border-radius: 50px;    cursor: pointer;    display: inline-block;    font-family: arial;    font-weight: bold;    position: absolute;    top: -20px;    right: -20px;    font-size: 25px;    line-height: 30px;    width: 30px;    height: 30px;    text-align: center;} .popupCloseButton:hover {    background-color: #ccc;} .trigger_popup_fricc {    cursor: pointer;    font-size: 20px;    margin: 20px;    display: inline-block;    font-weight: bold;}/* Popup box BEGIN */';
+style.innerHTML = '/* Popup box BEGIN */ .hover_bkgr_fricc{ background:rgba(0,0,0,.4); cursor:pointer; display:none; height:100%; position:fixed; text-align:center; top:-5px; width:100%; z-index:10000;}.hover_bkgr_fricc .helper{ display:inline-block; height:100%; vertical-align:middle;}.hover_bkgr_fricc > div {    background-color: #fff;    box-shadow: 10px 10px 60px #555; display: inline-block; height: auto; max-width: 1000px; min-height: 100px; vertical-align: middle; width: 60%; position: relative; border-radius: 8px; padding: 15px 5%;} .popupCloseButton {    background-color: #fff;    border: 3px solid #999;    border-radius: 50px;    cursor: pointer;    display: inline-block;    font-family: arial;    font-weight: bold;    position: absolute;    top: -20px;    right: -20px;    font-size: 25px;    line-height: 30px;    width: 30px;    height: 30px;    text-align: center;} .popupCloseButton:hover {    background-color: #ccc;} .trigger_popup_fricc {    cursor: pointer;    font-size: 20px;    margin: 20px;    display: inline-block;    font-weight: bold;}/* Popup box BEGIN */';
 document.getElementsByTagName('head')[0].appendChild(style);
 
 let popup_html = '<span class="helper"></span><div style="width: 800px;">'
@@ -51,7 +51,7 @@ let popup_html = '<span class="helper"></span><div style="width: 800px;">'
                 + '<div style="width: 100%; height: 100%; border:0px solid black; padding: 2px; text-align: left;">'
                 + '<table>'
                 +   '<tr>'
-                +     '<td>'
+                +     '<td style="overflow-y: scroll;max-width: 700px; word-wrap: break-word;">'
                 +       (typeof message === 'object' ? JSON.stringify(message) : message)
                 +     '</td>'
                 +   '</tr>'
