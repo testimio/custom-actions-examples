010321b8bc7b1168023ef71fb6143d2d551d42da
Barry David Solomon <barrysolomon@hotmail.com>
2022-01-24 11:45:33 -0700

    Added Cookie/Session Clear

---
+++ testim-created/browser/cookies-session-clear.js
@@ -0,0 +1,46 @@
+/**
+ *  Cookies-Session Clear
+ *
+ *      Clears all cookies, sessionStorage and localStorage
+ * 
+ *  Parameters:
+ *     
+ *  Notes
+ * 
+ *      https://stackoverflow.com/questions/179355/clearing-all-cookies-with-javascript
+ * 
+ *  Disclaimer
+ *      This Custom Action is provided "AS IS".  It is for instructional purposes only and is not officially supported by Testim
+  * 
+ *  Base Step
+ *      Custom Action
+ * 
+ *  Installation
+ *      Create a new "Custom Action" named "Cookies-Session Clear"
+ *      Set the new custom action's function body to this javascript
+ *      Exit the step editor
+ *      Save the test
+ *      Bob's your uncle
+ *
+**/
+
+(function () {
+    var cookies = document.cookie.split("; ");
+    for (var c = 0; c < cookies.length; c++) {
+        var d = window.location.hostname.split(".");
+        while (d.length > 0) {
+            var cookieBase = encodeURIComponent(cookies[c].split(";")[0].split("=")[0]) + '=; expires=Thu, 01-Jan-1970 00:00:01 GMT; domain=' + d.join('.') + ' ;path=';
+            var p = location.pathname.split('/');
+            document.cookie = cookieBase + '/';
+            while (p.length > 0) {
+                document.cookie = cookieBase + p.join('/');
+                p.pop();
+            };
+            d.shift();
+        }
+    }
+})();
+
+window.localStorage.clear();
+
+window.sessionStorage.clear();
