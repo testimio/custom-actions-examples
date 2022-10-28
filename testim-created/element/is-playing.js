de8c729a44a3cb66f2eee801e806eab3bbb6b5a3
Barry David Solomon <barrysolomon@hotmail.com>
2022-10-28 11:46:05 -0600

    Updated to support different media element

--- testim-created/element/is-playing.js
+++ testim-created/element/is-playing.js
@@ -11,6 +11,11 @@
  *  Base Step
  *      Custom Validation
  *  
+ *  Notes
+ * 
+ *  Version       Date          Author          Details
+ *      1.1.0     10/28/2022    Barry Solomon   Handle nested player and some new weird methods instead of properties found for a specific player
+ *  
  *  Disclaimer
  *      This Custom Action is provided "AS IS".  It is for instructional purposes only and is not officially supported by Testim
  * 
@@ -18,7 +23,13 @@
 
  function IsPlaying(element, expectedIsPlaying) {
 
-    let actual_isplaying = !!(element.currentTime > 0 && !element.paused && !element.ended && element.readyState > 2) ? true : false;
+    let current_time = element.currentTime ?? element?.player?.currentTime();
+    let has_started  = element.hasStarted ?? element?.player?.hasStarted();
+    let ready_state  = element.readyState ?? element?.player?.readyState();
+    let paused       = element.paused ?? element?.player?.paused();
+    let ended        = element.ended ?? element?.player?.ended();
+    
+    let actual_isplaying = !!(current_time > 0 && !paused && !ended && ready_state > 2 && has_started !== false) ? true : false;
 
     console.log("actual_isplaying", actual_isplaying, "expectedIsPlaying", expectedIsPlaying);
 
@@ -34,6 +45,14 @@
 if (typeof (element) === 'undefined' || element === null)
     throw new Error("Target element has not been specified.");
 
+var video_element = element;
+if (video_element.tagName.toLowerCase() === 'div') {
+    video_element = element.querySelector("player");
+    if (video_element === null) {
+        video_element = element.querySelector("video");
+    }
+}
+
 let is_playing = (typeof isPlaying !== 'undefined' && isPlaying !== null) ? isPlaying : true;
 
-return IsPlaying(element, is_playing);
+return IsPlaying(video_element, is_playing);
