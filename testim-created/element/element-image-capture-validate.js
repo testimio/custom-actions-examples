57c88d56ea09694f97144acc3eb2475f6c3835af
Barry David Solomon <barrysolomon@hotmail.com>
2022-10-31 10:04:18 -0600

    Added image tolerance to validation

--- testim-created/element/element-image-capture-validate.js
+++ testim-created/element/element-image-capture-validate.js
@@ -1,7 +1,7 @@
 /**
  *  Element - Image Capture-Validate
  * 
- *      Capture an image of a DOM element and optionally validate
+ *      Capture an image of a DOM element and optionally validate (pixel exact match)
  * 
  *  Parameters
  *  
@@ -22,7 +22,7 @@
  *      If the dom element is an image and has a width/height set then this only gets the width/height part of the original image (if that makes sense to you)
  * 
  *  Version       Date          Author          Details
- *      2.0.0     08/26/2022    Barry Solomon   Added difference image and pixel tollerance 
+ *      2.0.0     08/26/2022    Barry Solomon   Added difference image
  *  
  *  Disclaimer
  *      This Custom Action is provided "AS IS".  It is for instructional purposes only and is not officially supported by Testim
@@ -36,7 +36,7 @@
 // http://www.github.com/HumbleSoftware/js-imagediff
 // https://ourcodeworld.com/articles/read/447/how-to-differentiate-2-images-with-javascript-using-js-imagediff
 
-(function (e, t) { var n = this; if (typeof module != "undefined") { var r; try { Canvas = require("canvas") } catch (i) { } module.exports = t(n, e, Canvas) } else typeof define == "function" && typeof define.amd == "object" ? define(t(n, e)) : n[e] = t(n, e) })("imagediff", function (t, n, r) { function v(n, i) { var s; if (r) s = r.createCanvas(n, i); else { if (!t.document || !t.document.createElement) throw new Error(e.message + "\n" + "Please see https://github.com/HumbleSoftware/js-imagediff#cannot-find-module-canvas\n"); s = document.createElement("canvas"), n && (s.width = n), i && (s.height = i) } return s } function m(e, t) { return l.width = e, l.height = t, c.clearRect(0, 0, e, t), c.createImageData(e, t) } function g() { return r } function y(e) { return x(e, u) } function b(e) { return x(e, s) } function w(e) { return x(e, o) } function E(e) { return !!e && !!x(e, a) && typeof e.width !== f && typeof e.height !== f && typeof e.data !== f } function S(e) { return y(e) || b(e) || w(e) || E(e) } function x(e, t) { return typeof e == "object" && !!Object.prototype.toString.apply(e).match(t) } function T(e) { var t = e.height, n = e.width, r = e.data, i, s, o; l.width = n, l.height = t, i = c.getImageData(0, 0, n, t), s = i.data; for (o = e.data.length; o--;)s[o] = r[o]; return i } function N(e) { if (y(e)) return C(e); if (b(e)) return k(e); if (w(e)) return L(e); if (E(e)) return e } function C(e) { var t = e.height, n = e.width; return l.width = n, l.height = t, c.clearRect(0, 0, n, t), c.drawImage(e, 0, 0), c.getImageData(0, 0, n, t) } function k(e) { var t = e.height, n = e.width, r = e.getContext("2d"); return r.getImageData(0, 0, n, t) } function L(e) { var t = e.canvas, n = t.height, r = t.width; return e.getImageData(0, 0, r, n) } function A(e) { var t = N(e), n = v(t.width, t.height), r = n.getContext("2d"); return r.putImageData(t, 0, 0), n } function O(e, t) { return e.width === t.width } function M(e, t) { return e.height === t.height } function _(e, t) { return M(e, t) && O(e, t) } function D(e, t, n) { var r = e.data, i = t.data, s = r.length, o; n = n || 0; if (!_(e, t)) return !1; for (o = s; o--;)if (r[o] !== i[o] && Math.abs(r[o] - i[o]) > n) return !1; return !0 } function P(e, t, n) { return (_(e, t) ? H : B)(e, t, n) } function H(e, t, n) { var r = e.height, i = e.width, s = m(i, r), o = e.data, u = t.data, a = s.data, f = a.length, l, c, h, p, d, v; for (h = 0; h < f; h += 4)a[h] = Math.abs(o[h] - u[h]), a[h + 1] = Math.abs(o[h + 1] - u[h + 1]), a[h + 2] = Math.abs(o[h + 2] - u[h + 2]), a[h + 3] = Math.abs(255 - Math.abs(o[h + 3] - u[h + 3])); return s } function B(e, t, n) { function b(e) { f === "top" ? (l = 0, c = 0) : (l = Math.floor((r - e.height) / 2), c = Math.floor((i - e.width) / 2)) } var r = Math.max(e.height, t.height), i = Math.max(e.width, t.width), s = m(i, r), o = e.data, u = t.data, a = s.data, f = n && n.align, l, c, h, p, d, v, g, y; for (d = a.length - 1; d > 0; d -= 4)a[d] = 255; b(e); for (h = e.height; h--;)for (p = e.width; p--;)d = 4 * ((h + l) * i + (p + c)), v = 4 * (h * e.width + p), a[d + 0] = o[v + 0], a[d + 1] = o[v + 1], a[d + 2] = o[v + 2]; b(t); for (h = t.height; h--;)for (p = t.width; p--;)d = 4 * ((h + l) * i + (p + c)), v = 4 * (h * t.width + p), a[d + 0] = Math.abs(a[d + 0] - u[v + 0]), a[d + 1] = Math.abs(a[d + 1] - u[v + 1]), a[d + 2] = Math.abs(a[d + 2] - u[v + 2]); return s } function j() { var e; for (e = 0; e < arguments.length; e++)if (!S(arguments[e])) throw { name: "ImageTypeError", message: "Submitted object was not an image." } } function F(e, t) { return e = document.createElement(e), e && t && (e.innerHTML = t), e } function I(e, t) { return typeof document != "undefined" ? q(e, t) : R(e, t) } function q(e, t) { var n = F("div", "<span>Expected to be equal."), r = F("div", "<div>Actual:</div>"), i = F("div", "<div>Expected:</div>"), s = F("div", "<div>Diff:</div>"), o = p.diff(e, t), u = v(), a; return u.height = o.height, u.width = o.width, n.style.overflow = "hidden", r.style.float = "left", i.style.float = "left", s.style.float = "left", a = u.getContext("2d"), a.putImageData(o, 0, 0), r.appendChild(A(e)), i.appendChild(A(t)), s.appendChild(u), n.appendChild(r), n.appendChild(i), n.appendChild(s), n.innerHTML } function R(e, t) { return "Expected to be equal." } function U(e, t, n) { var r = A(e), i, s; n = n || Function, i = r.toDataURL().replace(/^data:image\/\w+;base64,/, ""), s = Buffer.from(i, "base64"), require("fs").writeFile(t, s, n) } var i = /\[object Array\]/i, s = /\[object (Canvas|HTMLCanvasElement)\]/i, o = /\[object CanvasRenderingContext2D\]/i, u = /\[object (Image|HTMLImageElement)\]/i, a = /\[object ImageData\]/i, f = "undefined", l = v(), c = l.getContext("2d"), h = t[n], p, d; return d = { toBeImageData: function () { return { compare: function (e, t) { var n = p.isImageData(e); return { pass: n, message: n ? "Is ImageData" : "Is not ImageData" } } } }, toImageDiffEqual: function () { return { compare: function (e, t, n) { var r = p.equal(e, t, n); return { pass: r, message: r ? "Expected not to be equal." : I(e, t) } } } } }, p = { createCanvas: v, createImageData: m, getCanvasRef: g, isImage: y, isCanvas: b, isContext: w, isImageData: E, isImageType: S, toImageData: function (e) { return j(e), E(e) ? T(e) : N(e) }, equal: function (e, t, n) { return j(e, t), e = N(e), t = N(t), D(e, t, n) }, diff: function (e, t, n) { return j(e, t), e = N(e), t = N(t), P(e, t, n) }, jasmine: d, noConflict: function () { return t[n] = h, p } }, typeof module != "undefined" && (p.imageDataToPNG = U), p })
+(function (e, t) { var n = this; if (typeof module != "undefined") { var r; try { Canvas = require("canvas") } catch (i) { } module.exports = t(n, e, Canvas) } else typeof define == "function" && typeof define.amd == "object" ? define(t(n, e)) : n[e] = t(n, e) })("imagediff", function (t, n, r) { function v(n, i) { var s; if (r) s = r.createCanvas(n, i); else { if (!t.document || !t.document.createElement) throw new Error(e.message + "\n" + "Please see https://github.com/HumbleSoftware/js-imagediff#cannot-find-module-canvas\n"); s = document.createElement("canvas"), n && (s.width = n), i && (s.height = i) } return s } function m(e, t) { return l.width = e, l.height = t, c.clearRect(0, 0, e, t), c.createImageData(e, t) } function g() { return r } function y(e) { return x(e, u) } function b(e) { return x(e, s) } function w(e) { return x(e, o) } function E(e) { return !!e && !!x(e, a) && typeof e.width !== f && typeof e.height !== f && typeof e.data !== f } function S(e) { return y(e) || b(e) || w(e) || E(e) } function x(e, t) { return typeof e == "object" && !!Object.prototype.toString.apply(e).match(t) } function T(e) { var t = e.height, n = e.width, r = e.data, i, s, o; l.width = n, l.height = t, i = c.getImageData(0, 0, n, t), s = i.data; for (o = e.data.length; o--;)s[o] = r[o]; return i } function N(e) { if (y(e)) return C(e); if (b(e)) return k(e); if (w(e)) return L(e); if (E(e)) return e } function C(e) { var t = e.height, n = e.width; return l.width = n, l.height = t, c.clearRect(0, 0, n, t), c.drawImage(e, 0, 0), c.getImageData(0, 0, n, t) } function k(e) { var t = e.height, n = e.width, r = e.getContext("2d"); return r.getImageData(0, 0, n, t) } function L(e) { var t = e.canvas, n = t.height, r = t.width; return e.getImageData(0, 0, r, n) } function A(e) { var t = N(e), n = v(t.width, t.height), r = n.getContext("2d"); return r.putImageData(t, 0, 0), n } function O(e, t) { return e.width === t.width } function M(e, t) { return e.height === t.height } function _(e, t) { return M(e, t) && O(e, t) } function D(e, t, n) { var r = e.data, i = t.data, s = r.length, o; n = n || 0; if (!_(e, t)) return !1; for (o = s; o--;)if (r[o] !== i[o] && Math.abs(r[o] - i[o]) > n) return !1; return !0 } function P(e, t, n) { return (_(e, t) ? H : B)(e, t, n) } function H(e, t, n) { var r = e.height, i = e.width, s = m(i, r), o = e.data, u = t.data, a = s.data, f = a.length, l, c, h, p, d, v; for (h = 0; h < f; h += 4)a[h] = Math.abs(o[h] - u[h]), a[h + 1] = Math.abs(o[h + 1] - u[h + 1]), a[h + 2] = Math.abs(o[h + 2] - u[h + 2]), a[h + 3] = Math.abs(255 - Math.abs(o[h + 3] - u[h + 3])); return s } function B(e, t, n) { function b(e) { f === "top" ? (l = 0, c = 0) : (l = Math.floor((r - e.height) / 2), c = Math.floor((i - e.width) / 2)) } var r = Math.max(e.height, t.height), i = Math.max(e.width, t.width), s = m(i, r), o = e.data, u = t.data, a = s.data, f = n && n.align, l, c, h, p, d, v, g, y; for (d = a.length - 1; d > 0; d -= 4)a[d] = 255; b(e); for (h = e.height; h--;)for (p = e.width; p--;)d = 4 * ((h + l) * i + (p + c)), v = 4 * (h * e.width + p), a[d + 0] = o[v + 0], a[d + 1] = o[v + 1], a[d + 2] = o[v + 2]; b(t); for (h = t.height; h--;)for (p = t.width; p--;)d = 4 * ((h + l) * i + (p + c)), v = 4 * (h * t.width + p), a[d + 0] = Math.abs(a[d + 0] - u[v + 0]), a[d + 1] = Math.abs(a[d + 1] - u[v + 1]), a[d + 2] = Math.abs(a[d + 2] - u[v + 2]); return s } function j() { var e; for (e = 0; e < arguments.length; e++)if (!S(arguments[e])) throw { name: "ImageTypeError", message: "Submitted object was not an image." } } function F(e, t) { return e = document.createElement(e), e && t && (e.innerHTML = t), e } function I(e, t) { return typeof document != "undefined" ? q(e, t) : R(e, t) } function q(e, t) { var n = F("div", "<span>Expected to be equal."), r = F("div", "<div>Actual:</div>"), i = F("div", "<div>Expected:</div>"), s = F("div", "<div>Diff:</div>"), o = p.diff_image(e, t), u = v(), a; return u.height = o.height, u.width = o.width, n.style.overflow = "hidden", r.style.float = "left", i.style.float = "left", s.style.float = "left", a = u.getContext("2d"), a.putImageData(o, 0, 0), r.appendChild(A(e)), i.appendChild(A(t)), s.appendChild(u), n.appendChild(r), n.appendChild(i), n.appendChild(s), n.innerHTML } function R(e, t) { return "Expected to be equal." } function U(e, t, n) { var r = A(e), i, s; n = n || Function, i = r.toDataURL().replace(/^data:image\/\w+;base64,/, ""), s = Buffer.from(i, "base64"), require("fs").writeFile(t, s, n) } var i = /\[object Array\]/i, s = /\[object (Canvas|HTMLCanvasElement)\]/i, o = /\[object CanvasRenderingContext2D\]/i, u = /\[object (Image|HTMLImageElement)\]/i, a = /\[object ImageData\]/i, f = "undefined", l = v(), c = l.getContext("2d"), h = t[n], p, d; return d = { toBeImageData: function () { return { compare: function (e, t) { var n = p.isImageData(e); return { pass: n, message: n ? "Is ImageData" : "Is not ImageData" } } } }, toImageDiffEqual: function () { return { compare: function (e, t, n) { var r = p.equal(e, t, n); return { pass: r, message: r ? "Expected not to be equal." : I(e, t) } } } } }, p = { createCanvas: v, createImageData: m, getCanvasRef: g, isImage: y, isCanvas: b, isContext: w, isImageData: E, isImageType: S, toImageData: function (e) { return j(e), E(e) ? T(e) : N(e) }, equal: function (e, t, n) { return j(e, t), e = N(e), t = N(t), D(e, t, n) }, diff_image: function (e, t, n) { return j(e, t), e = N(e), t = N(t), P(e, t, n) }, jasmine: d, noConflict: function () { return t[n] = h, p } }, typeof module != "undefined" && (p.imageDataToPNG = U), p })
 
 /*!
  * html2canvas 1.3.2 <https://html2canvas.hertzen.com>
@@ -80,14 +80,16 @@ return new Promise(function (resolve, reject) {
         loadedImages++;
 
         // Skip execution of the callback if the 2 images have been not loaded
-        // Otherwise continue with the diff
+        // Otherwise continue with the diff_image
         if (loadedImages != 2) {
             return;
         }
 
-        if (ImageE !== undefined) {
+        imageMatch = imagediff.equal(ImageE, ImageA, image_tolerance);
 
-            imageMatch = imagediff.equal(ImageE, ImageA, image_tolerance);
+        var diff_image = imagediff.diff_image(ImageE, ImageA, image_tolerance);
+
+        if (ImageE !== undefined) {
 
             if (typeof displayImage !== 'undefined' && displayImage !== null && displayImage == true) {
 
@@ -155,37 +157,73 @@ return new Promise(function (resolve, reject) {
                 if (imageMatch !== undefined && imageMatch !== true) {
 
                     // Create the image that shows the difference between the images
-                    var diff = imagediff.diff(ImageE, ImageA, image_tolerance);
-                    var diff_img = imagediff.createCanvas(diff.width, diff.height);
-                    if (diff_img !== undefined) {
+                    var diff_canvas = imagediff.createCanvas(diff_image.width, diff_image.height);
+                    if (diff_canvas !== undefined) {
 
                         console.log("DISPLAY DIFFERENCE");
-                        diff_img.id = 'diffImageImg';
-                        diff_img.alt = "Diff Image";
+                        diff_canvas.id = 'diffImageImg';
+                        diff_canvas.alt = "Diff Image";
 
                         if (imageMatch) {
-                            diff_img.style = 'background-color: white;border:1px solid black; box-shadow: 0px 4px 2px 1px rgba(0, 255, 0, .2);';
+                            diff_canvas.style = 'background-color: white;border:1px solid black; box-shadow: 0px 4px 2px 1px rgba(0, 255, 0, .2);';
                         }
                         else {
                             console.log("UPDATE FAILING ACTUAL");
-                            diff_img.style = 'background-color: white;border:1px solid black; box-shadow: 0px 4px 2px 1px rgba(255, 0, 0, .2);';
-                            diff_img.style.boxShadow = '0px 4px 2px 1px rgba(255, 0, 0, .2)';
+                            diff_canvas.style = 'background-color: white;border:1px solid black; box-shadow: 0px 4px 2px 1px rgba(255, 0, 0, .2);';
+                            diff_canvas.style.boxShadow = '0px 4px 2px 1px rgba(255, 0, 0, .2)';
                         }
 
-                        diff_img.style.maxWidth = MAX_IMAGE_WIDTH + "px";
-                        diff_img.style.maxHeight = Math.floor(MAX_IMAGE_WIDTH * width_height_ratio); + "px";
+                        diff_canvas.style.maxWidth = MAX_IMAGE_WIDTH + "px";
+                        diff_canvas.style.maxHeight = Math.floor(MAX_IMAGE_WIDTH * width_height_ratio); + "px";
                         if (ImageE === undefined)
-                            diff_img.style.left = (left + 2 * (MAX_IMAGE_WIDTH + 10)) + 'px';
+                            diff_canvas.style.left = (left + 2 * (MAX_IMAGE_WIDTH + 10)) + 'px';
                         else
-                            diff_img.style.left = (left + 3 * (MAX_IMAGE_WIDTH + 10)) + 'px';
+                            diff_canvas.style.left = (left + 3 * (MAX_IMAGE_WIDTH + 10)) + 'px';
 
-                        diff_img.style.position = element_computed_style.position == 'fixed' ? 'fixed' : 'absolute';
-                        diff_img.style.top = element.offsetTop + 'px';
-                        diff_img.style.zIndex = 1000000;
+                        diff_canvas.style.position = element_computed_style.position == 'fixed' ? 'fixed' : 'absolute';
+                        diff_canvas.style.top = element.offsetTop + 'px';
+                        diff_canvas.style.zIndex = 1000000;
 
-                        var context = diff_img.getContext('2d');
-                        context.putImageData(diff, 0, 0);
-                        document.body.appendChild(diff_img);
+                        var context = diff_canvas.getContext('2d');
+
+                        let surface = diff_image; //context.getImageData(0, 0, diff_canvas.width, diff_canvas.height); 
+
+                        //Once you have the ImageData object you can to all your editing 
+                        for (let y = 0; y != surface.height; y++) {
+                            for (let x = 0; x != surface.width; x++) {
+                                //Remember this formula! 
+                                //i = (y * width + x) * depth 
+                                let i = (y * surface.width + x) * 4;
+                                //compute the average of RGB components 
+                                let intensity = (
+                                    surface.data[i + 0] +
+                                    surface.data[i + 1] +
+                                    surface.data[i + 2]
+                                ) / 3;
+                                //set the RGB components to the average. Each component 
+                                //has the same value so it is a shade of gray 
+                                if (surface.data[i + 0] > 0) {
+                                    surface.data[i + 0] = 255;
+                                    surface.data[i + 1] = 0; // intensity; 
+                                    surface.data[i + 2] = 0; // intensity; 
+                                }
+                            }
+                        }
+
+                        //Now you can put it back onto the canvas 
+                        context.putImageData(surface, 0, 0); //, surface.width, surface.height); 
+
+                        document.body.appendChild(diff_canvas);
+
+
+
+
+
+
+
+
+
+
 
                     }
 
@@ -212,7 +250,9 @@ return new Promise(function (resolve, reject) {
 
             exports.actualImageData = actualImageData;
             copyToClipboard(actualImageData);
-            console.log(actualImageData);
+
+            console.log("expectedImageData:", expectedImageData);
+            console.log("actualImageData:", actualImageData);
 
             // if no expected image data then pass
             //
